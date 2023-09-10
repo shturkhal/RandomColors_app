b7f9b553bdcafbdae0eaf6c53b280f752a820323
shturkhal <107053994+shturkhal@users.noreply.github.com>
2023-09-10 15:57:11 +0300

    append two new func

--- RandomColors/ColorsTableVC.swift
+++ RandomColors/ColorsTableVC.swift
@@ -7,25 +7,40 @@
 
 import UIKit
 
-class ColorsTableVC: UIViewController {
-  
-    
+// table views and collection views -  ALWAYS TAKE AN ARRAY!!!
 
+class ColorsTableVC: UIViewController {
+    
+    // table views and collection views -  ALWAYS TAKE AN ARRAY!!!
+    // create an empty array
+    var colors: [UIColor] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addRandomColors()
     }
     
- 
-    
+    // create lop for append random color in empty array
+    func addRandomColors() {
+        for _ in 0..<50 {
+            colors.append(createRandomColor())
+        } 
+    }
+    // create func that creates new random color
+    func createRandomColor() -> UIColor {
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1),
+                                  green: CGFloat.random(in: 0...1),
+                                  blue: CGFloat.random(in: 0...1),
+                                  alpha: 1)
+        return randomColor
+    }
     
     
     //MARK: example of perform segue
-//    @IBAction func tempButtonTapped(_ sender: UIButton) {
-//        // представити інший вʼю через ідентіфаер переходу
-//        performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)
-//    }
+    //    @IBAction func tempButtonTapped(_ sender: UIButton) {
+    //        // представити інший вʼю через ідентіфаер переходу
+    //        performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)
+    //    }
     
 }
 
@@ -39,7 +54,7 @@ extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
     }
-    
+    // did what when cell is selected
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)
     }
